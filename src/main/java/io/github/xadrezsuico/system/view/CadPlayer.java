4be44c10c7d399ee91ab5b54e0ad74d96dcb596d
cnvamaral <cnvamaral@WS-LAB08_143.univel.univel.br>
2017-03-24 20:41:31 -0300

    commit

--- src/main/java/io/github/xadrezsuico/system/view/CadPlayer.java
+++ src/main/java/io/github/xadrezsuico/system/view/CadPlayer.java
@@ -6,22 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
 import java.awt.GridBagLayout;
-import javax.swing.JSplitPane;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.border.BevelBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
-import javax.swing.JScrollPane;
-import javax.swing.JEditorPane;
-import javax.swing.JToggleButton;
-import javax.swing.JFormattedTextField;
-import javax.swing.JSpinner;
-import javax.swing.JSeparator;
-import java.awt.Choice;
+import javax.swing.DefaultComboBoxModel;
+import io.github.xadrezsuico.system.common.enums.Sex;
 
 public class CadPlayer extends JFrame {
 
@@ -29,6 +26,11 @@ public class CadPlayer extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
 
 	/**
 	 * Launch the application.
@@ -37,7 +39,7 @@ public class CadPlayer extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					CadPlayer frame = new CadPlayer();
+					CadReferee frame = new CadReferee();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -56,15 +58,15 @@ public class CadPlayer extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{144, 63, 147, 0};
+		gbl_contentPane.rowHeights = new int[]{96, 88, 79, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridheight = 3;
 		gbc_panel.gridwidth = 2;
 		gbc_panel.insets = new Insets(0, 0, 5, 5);
 		gbc_panel.fill = GridBagConstraints.BOTH;
@@ -72,13 +74,22 @@ public class CadPlayer extends JFrame {
 		gbc_panel.gridy = 0;
 		contentPane.add(panel, gbc_panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[]{76, 53, 59, 94, 52, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
+		JLabel lblStartN = new JLabel("Start N\u00BA:");
+		GridBagConstraints gbc_lblStartN = new GridBagConstraints();
+		gbc_lblStartN.anchor = GridBagConstraints.EAST;
+		gbc_lblStartN.insets = new Insets(0, 0, 5, 5);
+		gbc_lblStartN.gridx = 0;
+		gbc_lblStartN.gridy = 0;
+		panel.add(lblStartN, gbc_lblStartN);
+		
 		textField = new JTextField();
+		textField.setEditable(false);
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
@@ -86,6 +97,14 @@ public class CadPlayer extends JFrame {
 		gbc_textField.gridy = 0;
 		panel.add(textField, gbc_textField);
 		textField.setColumns(10);
+		
+		JLabel lblFirstName = new JLabel("First Name:");
+		GridBagConstraints gbc_lblFirstName = new GridBagConstraints();
+		gbc_lblFirstName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFirstName.anchor = GridBagConstraints.EAST;
+		gbc_lblFirstName.gridx = 0;
+		gbc_lblFirstName.gridy = 1;
+		panel.add(lblFirstName, gbc_lblFirstName);
 		
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
@@ -97,22 +116,142 @@ public class CadPlayer extends JFrame {
 		panel.add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
 		
-		JLabel lblLastName = new JLabel("Last Name");
+		JLabel lblLastName = new JLabel("Last Name:");
 		GridBagConstraints gbc_lblLastName = new GridBagConstraints();
 		gbc_lblLastName.anchor = GridBagConstraints.EAST;
-		gbc_lblLastName.insets = new Insets(0, 0, 0, 5);
+		gbc_lblLastName.insets = new Insets(0, 0, 5, 5);
 		gbc_lblLastName.gridx = 0;
 		gbc_lblLastName.gridy = 2;
 		panel.add(lblLastName, gbc_lblLastName);
 		
 		textField_2 = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-		gbc_textField_2.gridwidth = 4;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridwidth = 4;
 		gbc_textField_2.gridx = 1;
 		gbc_textField_2.gridy = 2;
 		panel.add(textField_2, gbc_textField_2);
 		textField_2.setColumns(10);
+		
+		JLabel lblSex = new JLabel("Sex:");
+		GridBagConstraints gbc_lblSex = new GridBagConstraints();
+		gbc_lblSex.anchor = GridBagConstraints.EAST;
+		gbc_lblSex.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSex.gridx = 0;
+		gbc_lblSex.gridy = 3;
+		panel.add(lblSex, gbc_lblSex);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBackground(Color.WHITE);
+		comboBox.setModel(new DefaultComboBoxModel(Sex.values()));
+		comboBox.setToolTipText("");
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.gridwidth = 2;
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 3;
+		panel.add(comboBox, gbc_comboBox);
+		
+		JLabel lblBirthDate = new JLabel("Birth Date:");
+		GridBagConstraints gbc_lblBirthDate = new GridBagConstraints();
+		gbc_lblBirthDate.anchor = GridBagConstraints.EAST;
+		gbc_lblBirthDate.insets = new Insets(0, 0, 5, 5);
+		gbc_lblBirthDate.gridx = 0;
+		gbc_lblBirthDate.gridy = 4;
+		panel.add(lblBirthDate, gbc_lblBirthDate);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.gridwidth = 2;
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 1;
+		gbc_textField_3.gridy = 4;
+		panel.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblRegistryNumber = new JLabel("Registration document:");
+		GridBagConstraints gbc_lblRegistryNumber = new GridBagConstraints();
+		gbc_lblRegistryNumber.anchor = GridBagConstraints.EAST;
+		gbc_lblRegistryNumber.insets = new Insets(0, 0, 5, 5);
+		gbc_lblRegistryNumber.gridx = 0;
+		gbc_lblRegistryNumber.gridy = 5;
+		panel.add(lblRegistryNumber, gbc_lblRegistryNumber);
+		
+		textField_4 = new JTextField();
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.gridwidth = 2;
+		gbc_textField_4.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 1;
+		gbc_textField_4.gridy = 5;
+		panel.add(textField_4, gbc_textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblCity = new JLabel("City:");
+		GridBagConstraints gbc_lblCity = new GridBagConstraints();
+		gbc_lblCity.anchor = GridBagConstraints.EAST;
+		gbc_lblCity.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCity.gridx = 0;
+		gbc_lblCity.gridy = 6;
+		panel.add(lblCity, gbc_lblCity);
+		
+		textField_5 = new JTextField();
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.gridwidth = 2;
+		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_5.gridx = 1;
+		gbc_textField_5.gridy = 6;
+		panel.add(textField_5, gbc_textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Registry Number");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 7;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textField_7 = new JTextField();
+		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
+		gbc_textField_7.gridwidth = 2;
+		gbc_textField_7.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_7.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_7.gridx = 1;
+		gbc_textField_7.gridy = 7;
+		panel.add(textField_7, gbc_textField_7);
+		textField_7.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Category:");
+		lblNewLabel.setToolTipText("Female\r\nMale");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 8;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		textField_6 = new JTextField();
+		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
+		gbc_textField_6.gridwidth = 2;
+		gbc_textField_6.insets = new Insets(0, 0, 0, 5);
+		gbc_textField_6.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_6.gridx = 1;
+		gbc_textField_6.gridy = 8;
+		panel.add(textField_6, gbc_textField_6);
+		textField_6.setColumns(10);
+		
+		JCheckBox chckbxMain = new JCheckBox("Main");
+		GridBagConstraints gbc_chckbxMain = new GridBagConstraints();
+		gbc_chckbxMain.anchor = GridBagConstraints.WEST;
+		gbc_chckbxMain.insets = new Insets(0, 0, 0, 5);
+		gbc_chckbxMain.gridx = 3;
+		gbc_chckbxMain.gridy = 8;
+		panel.add(chckbxMain, gbc_chckbxMain);
 	}
 
 }
--- src/main/java/io/github/xadrezsuico/system/view/teste.java
+++
@@ -1,5 +0,0 @@
-package io.github.xadrezsuico.system.view;
-
-public class teste {
-
-}
