d70a9def71967c938adf1e3d6be33fa5436e26d9
João Paulo Polles <joaopaulopolles@gmail.com>
2017-03-24 19:58:23 -0300

    Adicionado os Titles necessários.

--- src/main/java/io/github/xadrezsuico/system/common/enums/Title.java
+++ src/main/java/io/github/xadrezsuico/system/common/enums/Title.java
@@ -1,7 +1,18 @@
 package io.github.xadrezsuico.system.common.enums;
 
 public enum Title {
-	CM("Candidate Master", Sex.MALE);
+	
+	// Male Titles
+	CM("Candidate Master", Sex.MALE),
+	FM("FIDE Master", Sex.MALE),
+	IM("International Master", Sex.MALE),
+	GM("Grandmaster", Sex.MALE),
+	
+	// Woman Titles
+	WCM("Woman Candidate Master", Sex.FEMALE),
+	WFM("Woman FIDE Master", Sex.FEMALE),
+	WIM("Woman International Master", Sex.FEMALE),
+	WGM("Woman Grandmaster", Sex.FEMALE);
 	
 	private String name;
 	private Sex sex;
@@ -9,5 +20,12 @@ public enum Title {
 	private Title(String name, Sex sex){
 		this.name = name;
 		this.sex = sex;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	public Sex getSex(){
+		return this.sex;
 	}
 }
