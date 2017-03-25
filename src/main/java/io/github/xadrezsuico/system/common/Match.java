f005129a2b946da703f95959e60f6ffe067eef7f
João Paulo Polles <joaopaulopolles@gmail.com>
2017-03-24 21:32:43 -0300

    Match

--- src/main/java/io/github/xadrezsuico/system/common/Match.java
+++ src/main/java/io/github/xadrezsuico/system/common/Match.java
@@ -1,22 +1,20 @@
 package io.github.xadrezsuico.system.common;
 
 public class Match {
-	private Player player1;
-	private Player player2;
+	private final Player player1;
+	private final Player player2;
 	private int result;
 	
+	public Match(Player player1, Player player2){
+		this.player1 = player1;
+		this.player2 = player2;
+	}
 	
 	public Player getPlayer1() {
 		return player1;
 	}
-	public void setPlayer1(Player player1) {
-		this.player1 = player1;
-	}
 	public Player getPlayer2() {
 		return player2;
-	}
-	public void setPlayer2(Player player2) {
-		this.player2 = player2;
 	}
 	public int getResult() {
 		return result;
