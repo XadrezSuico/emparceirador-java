f61192b6e7ea50141b047cdfd1bcc355a00d6c65
estevam98 <jhonatanm222@gmail.com>
2017-03-28 01:10:41 -0300

    Classe que implementa os metodos DAO.

---
+++ src/main/java/io/github/xadrezsuico/system/persistence/AbstractDAO.java
@@ -0,0 +1,89 @@
+package io.github.xadrezsuico.system.persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import io.github.xadrezsuico.system.model.Entity;
+
+public abstract class AbstractDAO <T extends Entity> implements Abstract<T>{
+
+	private EntityManager entityManager;
+	
+	protected Class<T> entityClass;
+	
+	public AbstractDAO(){
+	}
+	
+	public AbstractDAO(Class<T> entityClass){
+		this.entityClass = entityClass;
+	}
+	
+	public EntityManager getEntityManager(){
+		if(entityManager == null){
+			EntityManagerFactory factory = Persistence.createEntityManagerFactory(null);
+			
+			entityManager = factory.createEntityManager();
+		}
+		return entityManager;
+	}
+	
+	@Override
+	public void toSave(T entity){
+		if(entity == null){
+			return;
+		}
+		
+		EntityManager entityMan = getEntityManager();
+		EntityTransaction entityTransaction = entityMan.getTransaction();
+		
+		entityTransaction.begin();
+		
+		if(entity.getId() != null){
+			entityMan.merge(entity);
+		}else{
+			entityMan.persist(entity);
+		}
+		
+		entityTransaction.commit();
+	}
+	
+	@Override
+	public T update(T entity){
+		if(entity == null){
+			return null;
+		}
+		
+		EntityManager entityMan = getEntityManager();
+		EntityTransaction entityTransaction = entityMan.getTransaction();
+		
+		entityTransaction.begin();
+		
+		if (entity.getId() != null) {
+			entity = entityMan.merge(entity);
+		}else{
+			entityMan.persist(entity);
+		}
+		
+		entityTransaction.commit();
+		
+		return entity;
+	}
+	
+	@Override
+	public void remove(T entity){
+		if(entity == null){
+			return;
+		}
+		
+		EntityManager entityMan = getEntityManager();
+		EntityTransaction entityTransaction = entityMan.getTransaction();
+		
+		entityTransaction.begin();
+		
+		entityMan.remove(entity);
+		
+		entityTransaction.commit();
+	}
+}
