11ee2d37d39c893c8d4857d1d03d4082fb4829ea
VLBPANOSSO <VLBPANOSSO@WS-LAB08_142.univel.univel.br>
2017-04-07 21:01:26 -0300

    Tela torneio

---
+++ src/main/java/io/github/xadrezsuico/system/view/TelaTorneio.java
@@ -0,0 +1,68 @@
+package io.github.xadrezsuico.system.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTabbedPane;
+import java.awt.GridBagConstraints;
+
+public class TelaTorneio extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaTorneio frame = new TelaTorneio();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaTorneio() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		GridBagConstraints gbc_tabbedPane = new GridBagConstraints();
+		gbc_tabbedPane.gridheight = 3;
+		gbc_tabbedPane.gridwidth = 2;
+		gbc_tabbedPane.fill = GridBagConstraints.BOTH;
+		gbc_tabbedPane.gridx = 0;
+		gbc_tabbedPane.gridy = 0;
+		contentPane.add(tabbedPane, gbc_tabbedPane);
+		
+		JPanel tab_lista_incritos = new JPanel();
+		tabbedPane.addTab("Inscritos", null, tab_lista_incritos, null);
+		
+		JPanel tab_rodada = new JPanel();
+		tabbedPane.addTab("Rodada", null, tab_rodada, null);
+		
+		JPanel tab_classificaco = new JPanel();
+		tabbedPane.addTab("Classifica\u00E7\u00E3o", null, tab_classificaco, null);
+	}
+
+}
