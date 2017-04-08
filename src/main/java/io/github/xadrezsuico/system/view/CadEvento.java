641fb906aeaae699ec0b60dc941accffc8a145a2
VLBPANOSSO <VLBPANOSSO@WS-LAB08_142.univel.univel.br>
2017-04-07 21:52:40 -0300

    Tela cadastro de evento

---
+++ src/main/java/io/github/xadrezsuico/system/view/CadEvento.java
@@ -0,0 +1,126 @@
+package io.github.xadrezsuico.system.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JButton;
+
+public class CadEvento extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadEvento frame = new CadEvento();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CadEvento() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 388, 169);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		GridBagConstraints gbc_lblNome = new GridBagConstraints();
+		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNome.anchor = GridBagConstraints.EAST;
+		gbc_lblNome.gridx = 0;
+		gbc_lblNome.gridy = 0;
+		contentPane.add(lblNome, gbc_lblNome);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 5;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblDataInicio = new JLabel("Data Inicio:");
+		GridBagConstraints gbc_lblDataInicio = new GridBagConstraints();
+		gbc_lblDataInicio.anchor = GridBagConstraints.EAST;
+		gbc_lblDataInicio.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDataInicio.gridx = 0;
+		gbc_lblDataInicio.gridy = 1;
+		contentPane.add(lblDataInicio, gbc_lblDataInicio);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		contentPane.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblDataFim = new JLabel("Data Fim:");
+		GridBagConstraints gbc_lblDataFim = new GridBagConstraints();
+		gbc_lblDataFim.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDataFim.anchor = GridBagConstraints.EAST;
+		gbc_lblDataFim.gridx = 3;
+		gbc_lblDataFim.gridy = 1;
+		contentPane.add(lblDataFim, gbc_lblDataFim);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.gridwidth = 2;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 4;
+		gbc_textField_2.gridy = 1;
+		contentPane.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		GridBagConstraints gbc_btnCancelar = new GridBagConstraints();
+		gbc_btnCancelar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnCancelar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCancelar.gridx = 1;
+		gbc_btnCancelar.gridy = 3;
+		contentPane.add(btnCancelar, gbc_btnCancelar);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		GridBagConstraints gbc_btnSalvar = new GridBagConstraints();
+		gbc_btnSalvar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSalvar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSalvar.gridwidth = 2;
+		gbc_btnSalvar.gridx = 4;
+		gbc_btnSalvar.gridy = 3;
+		contentPane.add(btnSalvar, gbc_btnSalvar);
+	}
+
+}
